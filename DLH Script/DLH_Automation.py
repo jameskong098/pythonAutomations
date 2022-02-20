2ad769572e47eb72beacf6d5c4a1c2069c765f3a
James Kong <jameskong098@gmail.com>
2022-02-19 23:53:27 -0500

    finished the script, fully functional

--- DLH Script/DLH_Automation.py
+++ DLH Script/DLH_Automation.py
@@ -15,42 +15,38 @@ driver = webdriver.Chrome()
 
 driver.get("https://slate.brandeis.edu/portal/brandeis_covid19")
 
-login = driver.find_element(By.XPATH, '//*[@id="username"]')
-login.send_keys("[Enter username here]")
+driver.find_element(By.XPATH, '//*[@id="username"]').send_keys("[enter Brandeis username]") #Brandeis username should replace this string
 
-password = driver.find_element(By.XPATH, '//*[@id="password"]')
-password.send_keys("[Enter password here]")
+driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("[enter Brandeis password]") #Brandeis password should replace this string
 
-enter = driver.find_element(By.XPATH, '/html/body/div/div[3]/div/form/div[6]/button')
-enter.click()
+driver.find_element(By.XPATH, '/html/body/div/div[3]/div/form/div[6]/button').click()
 
-delay = 10
+time.sleep(5)
+
+iFrame = driver.find_element(By.XPATH, "//iframe[@id='duo_iframe']")
+driver.switch_to.frame(iFrame)
+
+maxTime = 10
 
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable(By.XPATH('//button[contains(text(),"Send Me a Push ")]'))).click()
-    print("found submit button!!!!!!")
+    WebDriverWait(driver, maxTime).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))).click()
 except TimeoutException:
-    print("Operation Timed Out!")
+    print("Duo Submit Timed Out!")
 
-dlh = driver.find_element(By.XPATH, '//*[@id="part_f279cdf7-5861-4d4d-b429-e929dd0f19d3"]/form/div/span/input')
-dlh.click()
+try:
+    WebDriverWait(driver, maxTime).until(EC.element_to_be_clickable((By.XPATH, "//input[@value='Daily Health Assessment']"))).click()
+except TimeoutException:
+    print("Daily Health Assessment Button Timed Out!")
 
-answer1 = driver.find_element(By.XPATH, '//*[@id="form_febaa6cf-a91c-4512-9a62-1b4d9fc76087_10"]')
-answer1.click()
+driver.find_element(By.XPATH, '//*[@id="form_febaa6cf-a91c-4512-9a62-1b4d9fc76087_10"]').click()
 
-answer2 = driver.find_element(By.XPATH, '//*[@id="form_d18b4fe3-24e1-48fa-8275-eac56f2485fc_2"]')
-answer2.click()
+driver.find_element(By.XPATH, '//*[@id="form_d18b4fe3-24e1-48fa-8275-eac56f2485fc_2"]').click()
 
-answer3 = driver.find_element(By.XPATH, '//*[@id="form_7621d269-5d6c-4104-90c4-ea21e5c0a953_1"]')
-answer3.click()
+driver.find_element(By.XPATH, '//*[@id="form_7621d269-5d6c-4104-90c4-ea21e5c0a953_1"]').click()
 
-answer4 = driver.find_element(By.XPATH, '//*[@id="form_718644d8-2fac-43e8-9ee4-b292aafe4f45_1"]')
-answer4.click()
+driver.find_element(By.XPATH, '//*[@id="form_718644d8-2fac-43e8-9ee4-b292aafe4f45_1"]').click()
 
-answer5 = driver.find_element(By.XPATH, '//*[@id="form_32d2699d-af14-421e-b709-fb6270bdbf8e_container"]/div[4]/button')
-answer5.click()
-
-time.sleep(3)
+driver.find_element(By.XPATH, '//*[@id="form_32d2699d-af14-421e-b709-fb6270bdbf8e_container"]/div[4]/button').click()
 
 driver.quit()
 
